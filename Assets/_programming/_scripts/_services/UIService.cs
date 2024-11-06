24b6030c1696fd1db89a2b6bce10158fb85befee
Jesper Elmtoft <113169402+JesperE98@users.noreply.github.com>
2024-11-06 14:13:29 +0100

    Revert "Added: [UI]"
    This reverts commit b8f7ff57eb5f8ef1353b679fca16d72e1b2878a8.

--- Assets/_programming/_scripts/_services/UIService.cs
+++ Assets/_programming/_scripts/_services/UIService.cs
@@ -3,43 +3,30 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIService : MonoBehaviour {
-    [Header("Event References")] 
-    public EventService EventService;
-    
-    [Header("Input Reference")] 
-    // public Input Input;
     
     [Header("UI Elements")]
     [SerializeField] private Text _collectibleText;
 
-    private void Awake() {
-        Init();
-    }
-    
+
     private void Init() {
-        EventService = new EventService();
         SubscribeToEvents();
     }
 
-    
+    private void Start() {
+        _collectibleText.text = "x0";
+    }
 
     private void OnDestroy() {
         UnsubscribeFromEvents();
     }
 
     private void SubscribeToEvents() {
-        EventService.OnCollectibleCollectedTextUpdate += OnCollectibleCollectedTextUpdate;
+        
     }
-
-    private void UnsubscribeFromEvents() {
-        EventService.OnCollectibleCollectedTextUpdate -= OnCollectibleCollectedTextUpdate;
-
-    }
+    
+    private void UnsubscribeFromEvents() {}
 
     private void Update() {
         
-    }
-    void OnCollectibleCollectedTextUpdate(string text) {
-            _collectibleText.text = text;
     }
 }
